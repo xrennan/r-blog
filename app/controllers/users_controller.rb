79130d2a0aac827dde93a09d3ccc99a2fcbe9cb3
RTorres <rennan.jltorres@gmail.com>
2020-07-16 23:07:16 -0300

    editing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+    @user = User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -9,6 +12,16 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
