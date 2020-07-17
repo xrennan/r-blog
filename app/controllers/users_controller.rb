cd960e41300d993258ccfa441d5caa4920d6ba53
RTorres <rennan.jltorres@gmail.com>
2020-07-16 23:39:49 -0300

    show users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
