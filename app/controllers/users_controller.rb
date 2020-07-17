89551898c0d42c439fa31535ce9ae7cfc39a8053
RTorres <rennan.jltorres@gmail.com>
2020-07-17 00:30:44 -0300

    add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
