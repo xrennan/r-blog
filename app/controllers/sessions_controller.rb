91d48931f8a7df822a6f98ec66ada5b4c8a93f3c
RTorres <rennan.jltorres@gmail.com>
2020-08-05 10:48:38 -0300

    create and destroy user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,26 @@
 class SessionsController < ApplicationController
 
   def new
-    
+
   end
 
   def create
+    user = User.find_by(email: params[:sessions][:email.downcase])
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login"
+      render 'new'
+    end
 
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out "
+    redirect_to root_path
   end
 
 end
